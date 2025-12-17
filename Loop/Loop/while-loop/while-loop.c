4a721fef44fd4eb51b993912b092c091a2abbe4c
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-17 16:12:30 +0545

    Implement while loop examples in while-loop.c
    Added multiple C programs demonstrating the use of while loops for various tasks.

---
+++ Loop/Loop/while-loop/while-loop.c
@@ -0,0 +1,18 @@
+1. Print Numbers Using Two While Loops
+  Write a C program to print numbers from 0 to 10 and 10 to 0 using two while loops.
+
+  2. Sum of Positive Integers Until 0
+
+Write a C program that prompts the user to input a series of integers until the user stops entering 0 using a while loop.
+  Calculate and print the sum of all the positive integers entered.
+
+  3. Factorial Calculation Using a While Loop
+
+Write a C program that prompts the user to enter a positive integer. 
+It then calculates and prints the factorial of that number using a while loop.
+
+4. Palindrome Number Check Using a While Loop
+
+Write a C program that implements a program to check if a given number is a palindrome using a while loop.
+
+5. Fibonacci Sequence Generation Using a While Loop
