04f51ca819435a44570df363da7eb473037aaea5
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-17 15:15:12 +0545

    Update infinite-loop in for-loop.c

--- infinite-loop in for-loop.c
+++ infinite-loop in for-loop.c
@@ -13,10 +13,17 @@ int main () {						//run this program and see the output
 
 /*Initialization → empty → nothing happens
 
-Condition → empty → interpreted as true (by C standard)
+Condition → empty → interpreted as true (by C standard).
 
-Update → empty → nothing happens
+The compiler internally treats it like: for ( ; 1 ; )
 
-Because the condition is empty, the compiler treats it as always true.
+In a for loop, only the condition controls whether the loop continues.
 
-So the loop never ends, creating an infinite loop.*/
+When the condition part is empty, C treats it as 1 (true).
+
+The compiler does not require initialization or increment.
+
+Since the condition is always true, the loop never stops.
+Update → empty → nothing happens.
+
+So the loop never ends, creating an infinite loop.    */
