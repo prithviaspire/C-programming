cb34890a856364a18270117d8aa8fa82f7cfccd0
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-17 14:54:40 +0545

    Implement infinite for loop in main function
    Added an infinite for loop that prints a message.

---
+++ infinite-loop in for-loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+	// This is an infinite for loop
+	// as the condition expression
+	// is blank
+	for ( ; ; ) {
+		printf("This loop will run forever.");
+	}
+	return 0;
+}
