41237ca525f4a5e3c79e881fdc06ba682c55ead2
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-25 22:43:41 +0545

    Create whileand-do-while-loop.c

---
+++ whileand-do-while-loop.c
@@ -0,0 +1,21 @@
+//while loop
+
+#include <stdio.h>
+
+int main()  {
+	while (1)
+		printf("This loop will run forever.\n");
+	return 0;
+}
+
+//do-while-loop
+
+#include <stdio.h>
+
+int main() {
+	do {
+		printf("This loop will run forever.");
+	} while (1);
+
+	return 0;
+}
